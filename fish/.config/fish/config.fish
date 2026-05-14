ed0b67873a637a306def4ee9d889d5da158888bd
Christian Felipe Álvarez <calvarez@newrelic.com>
2026-05-14 09:19:35 +0200

    feat: disable fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,14 @@
+# Paths
 fish_add_path -U $HOME/.local/bin
 
+# Disable fish greeting
+set -g fish_greeting
+
+# Environment variables
 set -gx EDITOR nvim
 set -gx LANG "en_US.utf-8"
 
+# Tools setup
 if type -q zoxide
     zoxide init fish | source
 end
